dd3f582655293ff0ecea2b73af9ccac95c5fd0e8
Samarth Kamble <kamble.samarth91@gmail.com>
2024-03-21 01:46:51 +0530

    Title changed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Auth",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: "SoftThread Haven - Admin",
+  description: "Admin dashboard to manage SoftThread data",
 };
 
 export default function RootLayout({
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,8 +10,8 @@ import { ToasterProvider } from "@/lib/ToasterProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Dashboard",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: "SoftThread Haven - Admin",
+  description: "Admin dashboard to manage SoftThread data",
 };
 
 export default function RootLayout({
